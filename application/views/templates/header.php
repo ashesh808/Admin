836409c472b85f12257760c50e49ff10d3b409c2
asheshthecoder <37954173+asheshthecoder@users.noreply.github.com>
2018-04-10 11:13:41 +0545

    Model of Home Page

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -11,12 +11,21 @@
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="<?php echo base_url();?>includes/css/jquery-ui.css" rel="stylesheet" type="text/css"/>
     <link href="<?php echo base_url();?>includes/css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url();?>vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>includes/css/sb-admin-2.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>vendor/morrisjs/morris.css" rel="stylesheet">
+
 
     <!-- js -->
     <script type="text/javascript" src="<?php echo base_url();?>includes/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url();?>includes/js/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url();?>includes/js/jquery-ui.js"></script>
     <script type="text/javascript" src="<?php echo base_url();?>includes/js/script.js"></script>
+    <script src="<?php echo base_url();?>includes/js/sb-admin-2.js"></script>
+    <script src="<?php echo base_url();?>vendor/raphael/raphael.min.js"></script>
+    <script src="<?php echo base_url();?>vendor/morrisjs/morris.min.js"></script>
+    <script src="<?php echo base_url();?>includes/js/morris-data.js"></script>
+    <script src="<?php echo base_url();?>vendor/metisMenu/metisMenu.min.js"></script>
 
 </head>
 <body>

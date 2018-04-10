e547d46d6ffc3b3793144ea42454d9bb69176462
asheshthecoder <37954173+asheshthecoder@users.noreply.github.com>
2018-04-10 17:24:35 +0545

    JQuery Bug fixed

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,30 +4,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <title>NewsWebsite</title>
-    <link href="<?php echo base_url();?>img/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link href="<?php echo base_url(); ?>img/favicon.ico" rel="shortcut icon" type="image/x-icon">
 
     <!-- css -->
-    <link href="<?php echo base_url();?>includes/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="<?php echo base_url();?>includes/css/jquery-ui.css" rel="stylesheet" type="text/css"/>
-    <link href="<?php echo base_url();?>includes/css/style.css" rel="stylesheet" type="text/css"/>
-    <link href="<?php echo base_url();?>vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>includes/css/sb-admin-2.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>vendor/morrisjs/morris.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="<?php echo base_url(); ?>includes/css/jquery-ui.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url(); ?>includes/css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo base_url(); ?>vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>includes/css/sb-admin-2.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>vendor/morrisjs/morris.css" rel="stylesheet">
 
 
     <!-- js -->
-    <script type="text/javascript" src="<?php echo base_url();?>includes/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url();?>includes/js/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url();?>includes/js/jquery-ui.js"></script>
-    <script type="text/javascript" src="<?php echo base_url();?>includes/js/script.js"></script>
-    <script src="<?php echo base_url();?>includes/js/sb-admin-2.js"></script>
-    <script src="<?php echo base_url();?>vendor/raphael/raphael.min.js"></script>
-    <script src="<?php echo base_url();?>vendor/morrisjs/morris.min.js"></script>
-    <script src="<?php echo base_url();?>includes/js/morris-data.js"></script>
-    <script src="<?php echo base_url();?>vendor/metisMenu/metisMenu.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>includes/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>includes/js/jquery-ui.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>includes/js/script.js"></script>
+    <script src="<?php echo base_url(); ?>includes/js/sb-admin-2.js"></script>
+    <script src="<?php echo base_url(); ?>vendor/raphael/raphael.min.js"></script>
+    <script src="<?php echo base_url(); ?>vendor/morrisjs/morris.min.js"></script>
+    <script src="<?php echo base_url(); ?>includes/js/morris-data.js"></script>
+    <script src="<?php echo base_url(); ?>vendor/metisMenu/metisMenu.min.js"></script>
 
 </head>
 <body>
+<div id="wrapper">
+    <?php $this->load->view('templates/top_navbar'); ?>
+    <div id="page-wrapper">
 
-<?php $this->load->view('templates/navbar'); ?>
+
